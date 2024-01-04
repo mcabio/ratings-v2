6ec1bb3e2910ae32fc6145f2c8e4f1ffe21591f5
Meryl Lou Cabio <meryllou7@gmail.com>
2024-01-04 02:54:42 -0600

    Created multiple html files to reflect different browser routes

--- crud.py
+++ crud.py
@@ -15,11 +15,30 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """Return all movies"""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+
+    return Movie.query.get(movie_id)
+
+def all_users():
+    """Return all movies"""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+
+    return User.query.get(user_id)
+
 def create_rating(user, movie, score):
 
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
+
 
 
 
