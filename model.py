e7730800a7950ed1bd47635cd496a9f5600a84a2
Meryl Lou Cabio <meryllou7@gmail.com>
2024-01-03 22:10:06 -0600

    Added User, Movie, and Rating classes in model.py

--- model.py
+++ model.py
@@ -5,9 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
-
-
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
@@ -18,6 +15,57 @@ def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
 
     print("Connected to the db!")
 
+class User(db.Model):
+    """A user"""
+    
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    ratings = db.relationship('Rating', back_populates='user')
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+    
+class Movie(db.Model):
+    """A movie"""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    ratings = db.relationship('Rating', back_populates='movie')
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+    
+class Rating(db.Model):
+    """A movie rating"""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, 
+                        autoincrement=True, 
+                        primary_key=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    movie = db.relationship('Movie', back_populates='ratings')
+    user = db.relationship('User', back_populates='ratings')
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}'
 
 if __name__ == "__main__":
     from server import app
